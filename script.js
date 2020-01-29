e86e47547540f2f30a6b3c4f431e6b9e9720d78f
RamlahAziz <44732722+RamlahAziz@users.noreply.github.com>
2020-01-29 13:56:36 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+/**
+ * New script file
+ */
+
+/* global getAssetRegistry getFactory emit */
+
+/**
+ * Sample transaction processor function.
+ * @param {org.medrex.basic.updateRecord} tx The sample transaction instance.
+ * @transaction
+ */
+
+async function updateRecord(tx) {  // eslint-disable-line no-unused-vars
+
+    // Save the old value of the asset.
+    const oldDiag = tx.record.pDiag;
+
+    // Update the asset with the new value.
+    tx.record.pDiag = tx.newDiag;
+
+    // Get the asset registry for the asset.
+    const assetRegistry = await getAssetRegistry('org.medrex.basic.healthRecord');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.record);
+
+    // Emit an event for the modified asset.
+    let event = getFactory().newEvent('org.medrex.basic', 'updateEvent');
+    event.record = tx.record;
+    event.oldDiag = oldDiag;
+    event.newDiag = tx.newDiag;
+    emit(event);
+}
